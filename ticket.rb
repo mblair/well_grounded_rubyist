18aebfed7c9ffbe699f3d930bad30340d37d4d1e
Matt Blair <me@matthewblair.net>
2011-06-23 23:24:15 -0400

    String interpolation.

--- ticket.rb
+++ ticket.rb
@@ -24,13 +24,6 @@ def ticket.date
   "01/02/03"
 end
 
-print "This ticket is for: "
-print ticket.event + ", at "
-print ticket.venue + ", on "
-puts ticket.date + "."
-print "The performer is "
-puts ticket.performer + "."
-print "The seat is "
-print ticket.seat + ", "
-print "and it costs $"
-print "%.2f." % ticket.price
+puts "This ticket is for: #{ticket.event}, at #{ticket.venue}, 
+  on #{ticket.date}. The performer is #{ticket.performer}. The seat is 
+  #{ticket.seat}, and it costs $#{"%.2f." % ticket.price}"
